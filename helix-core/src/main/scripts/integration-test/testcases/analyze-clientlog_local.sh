d9fc4ec860b9d221a2bd2d4b3cb214e618adbd13
zzhang <zzhang@linkedin.com>
2012-05-25 18:46:04 -0700

    change test script

--- helix-core/src/main/scripts/integration-test/testcases/analyze-clientlog_local.sh
+++ helix-core/src/main/scripts/integration-test/testcases/analyze-clientlog_local.sh
@@ -8,18 +8,21 @@ echo -e "----------------------------------------"
 ls *process_start*.log -1rt | tail -n 1 | xargs grep "create\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "create\t\t " sum "\t " count "\t " sum/count}'
 
 # read stats
-ls *process_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "read\t\t " sum "\t " count "\t " sum/count}'
-ls *process_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | grep CURRENTSTATES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{ if(count > 0) print "  read CS\t " sum "\t " count "\t " sum/count; else print "  read CS\t " 0 }'
-ls *process_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | grep MESSAGES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read MSG\t " sum "\t " count "\t " sum/count}'
-ls *process_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | grep LIVEINSTANCES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read LI\t " sum "\t " count "\t " sum/count}'
-ls *process_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | grep STATEMODELDEFS | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read SM\t " sum "\t " count "\t " sum/count}'
+ls *process_start*.log -1rt | tail -n 1 | xargs grep "getData\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "read\t\t " sum "\t " count "\t " sum/count}'
+ls *process_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep CURRENTSTATES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{ if(count > 0) print "  read CS\t " sum "\t " count "\t " sum/count; else print "  read CS\t " 0 }'
+ls *process_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep MESSAGES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read MSG\t " sum "\t " count "\t " sum/count}'
+ls *process_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep LIVEINSTANCES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read LI\t " sum "\t " count "\t " sum/count}'
+ls *process_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep STATEMODELDEFS | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read SM\t " sum "\t " count "\t " sum/count}'
 
 # update stats
-ls *process_start*.log -1rt | tail -n 1 | xargs grep "update\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "update\t\t " sum "\t " count "\t " sum/count}'
-ls *process_start*.log -1rt | tail -n 1 | xargs grep "update\." | grep MESSAGES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  update MSG\t " sum "\t " count "\t " sum/count}'
-ls *process_start*.log -1rt | tail -n 1 | xargs grep "update\." | grep CURRENTSTATES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{ if (count > 0) print "  update CS\t " sum "\t " count "\t " sum/count; else print "  update CS\t " 0 }'
+#ls *process_start*.log -1rt | tail -n 1 | xargs grep "update\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "update\t\t " sum "\t " count "\t " sum/count}'
+#ls *process_start*.log -1rt | tail -n 1 | xargs grep "update\." | grep MESSAGES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  update MSG\t " sum "\t " count "\t " sum/count}'
+#ls *process_start*.log -1rt | tail -n 1 | xargs grep "update\." | grep CURRENTSTATES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{ if (count > 0) print "  update CS\t " sum "\t " count "\t " sum/count; else print "  update CS\t " 0 }'
 
-
+# write stats
+ls *process_start*.log -1rt | tail -n 1 | xargs grep "setData\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "write\t\t " sum "\t " count "\t " sum/count}'
+ls *process_start*.log -1rt | tail -n 1 | xargs grep "setData\." | grep CURRENTSTATES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{if (count > 0) print "  write CS\t " sum "\t " count "\t " sum/count; else print "  write CS\t " 0 }'
+ls *process_start*.log -1rt | tail -n 1 | xargs grep "setData\." | grep MESSAGES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{if (count > 0) print "  write MSG\t " sum "\t " count "\t " sum/count; else print "  write MSG\t " 0 }'
 
 # delete stats
 ls *process_start*.log -1rt | tail -n 1 | xargs grep "delete\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "delete\t\t " sum "\t "count "\t " sum/count}'
@@ -30,7 +33,7 @@ ls *process_start*.log -1rt | tail -n 1 | xargs grep "getChildren\." | sed 's/.*
 
 ls *process_start*.log -1rt | tail -n 1 | xargs grep "exists\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "exist\t\t " sum "\t " count "\t " sum/count}'
 
-ls *process_start*.log -1rt | tail -n 1 | xargs grep "getStat\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "getStat\t\t " sum "\t " count "\t " sum/count}'
+#ls *process_start*.log -1rt | tail -n 1 | xargs grep "getStat\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "getStat\t\t " sum "\t " count "\t " sum/count}'
 
 # controller stats
 echo -e "\ncontroller zk stats:"
@@ -43,27 +46,30 @@ ls *manager_start*.log -1rt | tail -n 1 | xargs grep "asyncCreate\. path" | grep
 ls *manager_start*.log -1rt | tail -n 1 | xargs grep "create\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "create\t\t " sum "\t " count "\t " sum/count}'
 
 # read stats
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "read\t\t " sum "\t " count "\t " sum/count}'
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | grep CURRENTSTATES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read CS\t " sum "\t " count "\t " sum/count}'
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | grep EXTERNALVIEW | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read EV\t " sum "\t " count "\t " sum/count}'
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | grep CONFIGS | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read CFG\t " sum "\t " count "\t " sum/count}'
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | grep LIVEINSTANCES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read LI\t " sum "\t " count "\t " sum/count}'
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | grep CONTROLLER | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read CTRL\t " sum "\t " count "\t " sum/count}'
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "zk-readData\." | grep MESSAGES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read MSG\t " sum "\t " count "\t " sum/count}'
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getData\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "read\t\t " sum "\t " count "\t " sum/count}'
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep CURRENTSTATES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read CS\t " sum "\t " count "\t " sum/count}'
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep EXTERNALVIEW | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{if (count > 0) print "  read EV\t " sum "\t " count "\t " sum/count; else print "  read EV\t " 0}'
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep CONFIGS | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read CFG\t " sum "\t " count "\t " sum/count}'
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep LIVEINSTANCES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read LI\t " sum "\t " count "\t " sum/count}'
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep CONTROLLER | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read CTRL\t " sum "\t " count "\t " sum/count}'
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep MESSAGES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read MSG\t " sum "\t " count "\t " sum/count}'
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getData\." | grep IDEALSTATES | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "  read IS\t " sum "\t " count "\t " sum/count}'
+
 
 # update stats
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "update\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "update\t\t " sum "\t " count "\t " sum/count}'
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "update\." | grep EXTERNALVIEW | sed 's/.*time/time/g' | awk -F: '{sum+=$2; count++}END{print "  update EV\t " sum "\t " count "\t " sum/count}'
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "update\." | grep CONTROLLER | sed 's/.*time/time/g' | awk -F: '{sum+=$2; count++}END{print "  update CTRL\t " sum "\t " count "\t " sum/count}'
+#ls *manager_start*.log -1rt | tail -n 1 | xargs grep "update\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "update\t\t " sum "\t " count "\t " sum/count}'
+#ls *manager_start*.log -1rt | tail -n 1 | xargs grep "update\." | grep EXTERNALVIEW | sed 's/.*time/time/g' | awk -F: '{sum+=$2; count++}END{if (count > 0) print "  update EV\t " sum "\t " count "\t " sum/count; else print "  update EV\t " 0 }'
+#ls *manager_start*.log -1rt | tail -n 1 | xargs grep "update\." | grep CONTROLLER | sed 's/.*time/time/g' | awk -F: '{sum+=$2; count++}END{print "  update CTRL\t " sum "\t " count "\t " sum/count}'
 
+# write stats
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "setData\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "write\t\t " sum "\t " count "\t " sum/count}'
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "setData\." | grep EXTERNALVIEW | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{if (count > 0) print "  write EV\t " sum "\t " count "\t " sum/count; else print "  write EV\t " 0 }'
 
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "delete\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "delete\t\t " sum "\t "count "\t " sum/count}'
-
+ls *manager_start*.log -1rt | tail -n 1 | xargs grep "delete\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{if (count > 0) print "delete\t\t " sum "\t "count "\t " sum/count; else print "delete\t\t 0" }'
 
 ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getChildren\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "readChild\t " sum "\t " count "\t " sum/count}'
 
-
 ls *manager_start*.log -1rt | tail -n 1 | xargs grep "exists\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "exist\t\t " sum "\t " count "\t " sum/count}'
 
-ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getStat\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "getStat\t\t " sum "\t " count "\t " sum/count}'
+#ls *manager_start*.log -1rt | tail -n 1 | xargs grep "getStat\." | sed 's/.*time/time/g'|  awk -F: '{sum+=$2; count++}END{print "getStat\t\t " sum "\t " count "\t " sum/count}'
 
