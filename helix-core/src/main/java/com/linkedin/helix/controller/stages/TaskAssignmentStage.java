205843c29c520e27194f490d46a1ec266402706d
zzhang <zzhang@apache.org>
2012-12-12 11:12:19 -0800

    comment out print

--- helix-core/src/main/java/com/linkedin/helix/controller/stages/TaskAssignmentStage.java
+++ helix-core/src/main/java/com/linkedin/helix/controller/stages/TaskAssignmentStage.java
@@ -131,9 +131,9 @@ public class TaskAssignmentStage extends AbstractBaseStage
 //      logger.info("Sending Message " + message.getMsgId() + " to " + message.getTgtName()
 //          + " transit " + message.getPartitionName() + "|" + message.getPartitionNames()
 //          + " from:" + message.getFromState() + " to:" + message.getToState());
-	System.out.println("Sending Message " + message.getMsgId() + " to " + message.getTgtName()
-          + " transit " + (message.getGroupMessageMode()==false? message.getPartitionName() : message.getPartitionNames())
-          + " from:" + message.getFromState() + " to:" + message.getToState());
+//	System.out.println("Sending Message " + message.getMsgId() + " to " + message.getTgtName()
+//          + " transit " + (message.getGroupMessageMode()==false? message.getPartitionName() : message.getPartitionNames())
+//          + " from:" + message.getFromState() + " to:" + message.getToState());
       keys.add(keyBuilder.message(message.getTgtName(), message.getId()));
     }
 
